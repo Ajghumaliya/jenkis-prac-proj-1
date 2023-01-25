8ee91fc6fbd1fbfee2d21e3c4c9b427f5d0ec633
Kaushik.J.G <amit2ghumaliya@gmail.com>
2023-01-25 17:45:02 +0530

    version_1.0

---
+++ free.py
@@ -0,0 +1,15 @@
+
+from flask import Flask
+
+app=Flask("_name_")
+# @app.route("/")
+# def kar():
+#     return render_template('index.html')
+
+@app.route("/")
+def test():
+    
+    return "kaushik jentibhai ghumaliya"
+
+app.run(debug=True)
+
